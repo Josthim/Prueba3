777a1d1766facb73ce0c7aad75f06aac493fc55a
Josthimx1 <110624763+Josthim@users.noreply.github.com>
2023-02-16 11:31:06 -0600

    Add files via upload

---
+++ Prueba3 Josthim/Prueba3/PjPrueba3/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace PjPrueba3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Inss Laboral");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Inss Patronal");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("IR");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Deducciones de Ley", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3});
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.treeView1.Location = new System.Drawing.Point(2, 12);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Nodo1";
+            treeNode1.Text = "Inss Laboral";
+            treeNode2.Name = "Nodo2";
+            treeNode2.Text = "Inss Patronal";
+            treeNode3.Name = "Nodo3";
+            treeNode3.Text = "IR";
+            treeNode4.Name = "Nodo0";
+            treeNode4.Text = "Deducciones de Ley";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode4});
+            this.treeView1.Size = new System.Drawing.Size(578, 97);
+            this.treeView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Digite su Codigo";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(216, 137);
+            this.maskedTextBox1.Mask = "999999999999999";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(79, 23);
+            this.maskedTextBox1.TabIndex = 2;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Digite su Ingreso Menual";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(216, 181);
+            this.maskedTextBox2.Mask = "99999999999999999";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(91, 23);
+            this.maskedTextBox2.TabIndex = 4;
+            this.maskedTextBox2.ValidatingType = typeof(int);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Inss Laboral";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(253, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Inss Patronal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(476, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "IR";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 267);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(111, 23);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(238, 267);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(107, 23);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(434, 267);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(102, 23);
+            this.textBox3.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(76, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 27);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(227, 337);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 27);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(390, 337);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 27);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Salir";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(582, 425);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TreeView treeView1;
+        private Label label1;
+        private MaskedTextBox maskedTextBox1;
+        private Label label2;
+        private MaskedTextBox maskedTextBox2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ Prueba3 Josthim/Prueba3/PjPrueba3/Form5.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sub, des, total, imp, cre;
+            if (treeView1.SelectedNode.Text=="")
+            {
+                MessageBox.Show("Seleccione una rama");
+            }
+            else if (maskedTextBox1.Text == "")
+            {
+                MessageBox.Show("No se aceptan espacios, digite el sub total");
+                maskedTextBox1.Focus();
+            }
+            else
+            {
+                sub = Convert.ToDouble(maskedTextBox1.Text);
+                if (treeView1.SelectedNode.Text.Equals("Contado"))
+                {
+                    checkBox1.Enabled = true;
+
+                    if (sub >= 100 && sub <= 1000)
+                    {
+                        if (checkBox1.Checked == true)
+                        {
+                            textBox3.Text = "";
+                            des = sub * 0.05;
+                            textBox1.Text = des.ToString();
+                            total = sub - des;
+                            textBox2.Text = total.ToString();
+                        }
+                        else
+                        {
+                            if (checkBox1.Checked == false)
+                            {
+                                textBox1.Text = "";
+                                imp = sub * 0.15;
+                                textBox3.Text = imp.ToString();
+                                total = sub + imp;
+                                textBox2.Text = total.ToString();
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        if (sub > 1000 && sub <= 3000)
+                        {
+                            if (checkBox1.Checked == true)
+                            {
+                                textBox3.Text = "";
+                                des = sub * 0.10;
+                                textBox1.Text = des.ToString();
+                                total = sub - des;
+                                textBox2.Text = total.ToString();
+                            }
+                            else
+                            {
+                                if (checkBox1.Checked == false)
+                                {
+                                    textBox1.Text = "";
+                                    imp = sub * 0.10;
+                                    textBox3.Text = imp.ToString();
+                                    total = sub + imp;
+                                    textBox2.Text = total.ToString();
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (sub >= 3001)
+                            {
+                                if (checkBox1.Checked == true)
+                                {
+                                    textBox3.Text = "";
+                                    des = sub * 0.12;
+                                    textBox1.Text = des.ToString();
+                                    total = sub - des;
+                                    textBox2.Text = total.ToString();
+                                }
+                                else
+                                {
+                                    if (checkBox1.Checked == false)
+                                    {
+                                        textBox1.Text = "";
+                                        imp = sub * 0.10;
+                                        textBox3.Text = imp.ToString();
+                                        total = sub + imp;
+                                        textBox2.Text = total.ToString();
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                }
+                else
+                {
+                    if (treeView1.SelectedNode.Text.Equals("Credito"))
+                    {
+                        
+                        checkBox1.Enabled = false;
+                        if (sub >= 100 && sub <= 5000)
+                        {
+                            checkBox1.Text = "";
+                            textBox1.Text = "";
+                            cre = sub * 0.05;
+                            textBox3.Text = cre.ToString();
+                            total = sub + cre;
+                            textBox2.Text = total.ToString();
+
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            textBox3.Enabled = false;
+            textBox2.Enabled = false;
+        }
+    }
+}
